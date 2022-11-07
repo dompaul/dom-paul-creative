13ce056dff6ff3e2e438ab770c3663218a9b6cf6
Dominic Paul <dominicbpaul@gmail.com>
2022-11-07 22:25:24 +0000

    [07/11/22] config update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /* next.config.js  */
 module.exports = {
   i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  images: {
+    domains: [
+      "scontent.cdninstagram.com",
+      "video.cdninstagram.com",
+      "scontent-lcy1-1.cdninstagram.com",
+      "video-lcy1-1.cdninstagram.com",
+    ],
   },
 };
