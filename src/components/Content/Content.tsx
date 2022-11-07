b8d59fc4d347c5d4d0a6986010d01a27f4ec59c7
Dominic Paul <dominicbpaul@gmail.com>
2022-11-07 10:49:35 +0000

    [07/11/22] update hero

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -3,19 +3,19 @@ import Image from "next/image";
 
 import { Mock } from "components/Mock";
 
-import frame from "../../assets/images/browser-frame.svg";
+// import frame from "../../assets/images/browser-frame.svg";
 
 import styles from "./Content.module.scss";
 
 export const Content: React.FC = ({ children }) => {
   return (
     <div className={cn(styles["content"])}>
-      {/* <Mock
+      <Mock
         type="video"
         url="/images/mock-website.webm"
         fallbackUrl="/images/mock-website.mp4"
         modifier="home"
-      /> */}
+      />
       <div
         className={cn(styles["content__section"], styles["content__wrapper"])}
       >
